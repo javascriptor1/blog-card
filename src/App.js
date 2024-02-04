87b490b7bbfe5192561f3917c258bcb0b8807ae4
Mohammed Fakih <mhd.fakih@gmail.com>
2024-02-04 19:31:29 +0300

    update folder structure + main compos

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 function App() {
   return (
-    <div className="App">
-      Learning Published 21 Dec 2023 HTML & CSS foundations These languages are
-      the backbone of every website, defining structure, content, and
-      presentation. Greg Hooper
-    </div>
+    <main className="container">
+      <img src="assets/images/illustration-article.svg" alt="" />
+      <p>Learning</p>
+      <p>
+        Published <time datetime="2023-12-21">21 Dec 2023</time>
+      </p>
+      <h1>
+        <abbr title="HyperText Markup Language">HTML</abbr> &
+        <abbr title="Cascading Style Sheets">CSS</abbr> foundations
+      </h1>
+      <p>
+        These languages are the backbone of every website, defining structure,
+        content, and presentation. Greg Hooper
+      </p>
+    </main>
   );
 }
 
